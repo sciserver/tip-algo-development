9c84a5e4ea9a80aa020e727f2ffa92d3816169ed
Ryan Hausen <ryan.hausen@gmail.com>
2024-02-29 21:23:37 -0500

    wip sciserver backend, elsevier stubbed out

--- src/backend/elsevier.py
+++ src/backend/elsevier.py
@@ -4,10 +4,9 @@ import numpy as np
 import scipy.sparse as sparse
 
 
-
-
 def get_data(year: int) -> Tuple[sparse.csr_matrix, np.ndarray, np.ndarray]:
     raise NotImplementedError("Not implemented in the Elsevier backend.")
+
 
 def update_posterior(auids: np.ndarray, posterior: np.ndarray, year: int) -> None:
     raise NotImplementedError("Not implemented in the Elsevier backend.")
--- src/backend/sci_server.py
+++ src/backend/sci_server.py
@@ -1,6 +1,7 @@
 import itertools
+import os
 import warnings
-from typing import Iterator, List, Tuple
+from typing import Callable, Iterable, Iterator, List, Tuple
 
 import numpy as np
 import pandas as pd
@@ -18,7 +19,11 @@ def extract_disconnected_auids(
     auids_to_explore = set(distinct_auids)
 
     for auid in filter(lambda i: i in auids_to_explore, distinct_auids):
-        connected_set = set(list(auid,))
+        connected_set = set(
+            list(
+                auid,
+            )
+        )
 
         eids = auid_eids[auid]
         auids = set(itertools.chain.from_iterable(eid_auids[eids].values))
@@ -35,6 +40,7 @@ def extract_disconnected_auids(
             new = connections - connected_set
 
         auids_to_explore -= connected_set
+
         yield list(connected_set)
 
 
@@ -45,30 +51,96 @@ def build_adjacency_matrix(
     weighted: bool = False,
 ) -> Tuple[np.ndarray, sparse.csr_matrix]:
 
+    if weighted:
+        raise NotImplementedError("Weighted adjacency matrix not implemented.")
+
     # this will be used as the index for assigning the row and column indices
     auids = np.sort(auids)
 
     # Build the matrix using COO format
     values, row_idxs, col_idxs = list(), list(), list()
-    for auid in auids:
+    for i, auid in enumerate(auids):
         eids = auid_eids[auid]
         co_auids = set(itertools.chain.from_iterable(eid_auids[eids].values))
-        co_auids = np.sort(co_auids- set([auid,]))
+        co_auids = np.sort(
+            co_auids
+            - set(
+                [
+                    auid,
+                ]
+            )
+        )
 
-        idxs = np.searchsorted(auids, co_auids)
+        col_idxs.extend(np.searchsorted(auids, co_auids).tolist())
+        row_idxs.extend([i] * len(co_auids))
+        values.extend([True] * len(co_auids))
+
+    A = sparse.coo_matrix(
+        (values, (row_idxs, col_idxs)),
+        shape=(len(auids), len(auids)),
+        dtype=bool,
+    ).tocsr()
+
+    return auids, A
 
 
+def calculate_prior_y(
+    auids: np.ndarray,
+    auid_eids: pd.Series,
+    eid_score: pd.Series,
+    year: int,
+    prior_y_aggregate_eid_score_func: Callable[[np.ndarray], float] = np.mean,
+    combine_posterior_prior_y_func: Callable[[np.ndarray], np.ndarray] = np.mean,
+) -> np.ndarray:
+
+    # get all of eids for each auid
+    selected_eids = auid_eids[auids]
+
+    prior_y = selected_eids.apply(
+        lambda eids: prior_y_aggregate_eid_score_func(eid_score[eids])
+    )
+
+    auid_eids[auids] = auid_eids[auids].apply(
+        lambda eids: eids.map(eids["score"]).values
+    )
+
+    prior_y_path = f"./data/prior_y_{year}.parquet"
+    if os.path.exists(prior_y_path):
+        posterior_y_t_minus_1 = pd.read_parquet(prior_y_path)[auids]
+        prior_y = combine_posterior_prior_y_func(
+            np.dstack([prior_y, posterior_y_t_minus_1]),
+            axis=1,
+        )
+
+    return prior_y
 
 
+def get_data(
+    year: int,
+    prior_y_aggregate_eid_score_func: Callable[[np.ndarray], float] = np.mean,
+    combine_posterior_prior_y_func: Callable[[np.ndarray], np.ndarray] = np.mean,
+) -> Iterable[Tuple[sparse.csr_matrix, np.ndarray, np.ndarray]]:
+    auid_eids = pd.read_parquet(f"./data/auid_eid_{year}.parquet")
+    eids = pd.read_parquet(f"./data/eid_{year}.parquet")
 
+    auid_eids = auid_eids.groupby("auid")["eid"].apply(list)  # auid -> eids
+    eid_auids = eid_auids.groupby("eid")["auid"].apply(list)  # eid -> auids
 
+    for connected_auids in extract_disconnected_auids(auid_eids, eid_auids):
+        auids, A = build_adjacency_matrix(
+            auid_eids,
+            eid_auids,
+            connected_auids,
+            False,
+        )
 
-# def get_data(year: int) -> Tuple[sparse.csr_matrix, np.ndarray, np.ndarray]:
-#     auid_eids = auid_eid_pairs.groupby("auid")["eid"].apply(list)  # auid -> eids
-#     eid_auids = auid_eid_pairs.groupby("eid")["auid"].apply(list)  # eid -> auids
+        prior_y = calculate_prior_y(
+            auids,
+            auid_eids,
+            eids,
+            year,
+            prior_y_aggregate_eid_score_func,
+            combine_posterior_prior_y_func,
+        )
 
-#     return (
-#         build_adjacency_matrix(auid_eid_pairs, logger),
-#         prior_y,
-#         posterior_y,
-#     )
+        yield A, auids, prior_y
